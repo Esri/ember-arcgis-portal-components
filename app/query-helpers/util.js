ed164320bbd006daa91510e290db6d1951cdc663
Ben Stoltz <bstoltz@esri.com>
2017-04-27 12:38:15 -0400

    Resolved issue with utils test

--- app/query-helpers/util.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-arcgis-portal-components/query-helpers/util';
--- tests/unit/utils/query-helpers-test.js
+++ tests/unit/utils/query-helpers-test.js
@@ -4,7 +4,27 @@ import { module, test } from 'qunit';
 module('Unit | Utility | query helpers');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = queryHelpers();
+test('it works', function (assert) {
+  const facet = {
+    name: 'Apps',
+    params: {
+      query: {
+        type: [
+          'Web Mapping Application'
+        ],
+        typekeywords: [
+          '-hubsite',
+          '-story'
+        ],
+        tags: [
+          '-survey',
+          '-storymap',
+          '-site'
+        ]
+      }
+    }
+  };
+  const query = 'Environment';
+  let result = queryHelpers.createQuery(facet, query);
   assert.ok(result);
 });
