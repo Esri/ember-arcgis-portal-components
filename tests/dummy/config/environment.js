b7531220d684b2516f2f889c0e9b4dd3e65d53e7
Dave Bouwman <dave@davebouwman.com>
2017-04-28 07:11:38 -0600

    missing gh deploy section

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -61,7 +61,6 @@ module.exports = function (environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.rootURL = '/ember-arcgis-portal-components/';
-
   }
 
   return ENV;
