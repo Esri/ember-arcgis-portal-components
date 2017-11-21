551c314a7cbbf112560cdab8394511b2ef628121
Brian Davidson <bdavidson@esri.com>
2017-11-21 12:26:40 -0500

    Add layer-picker test file

---
+++ tests/integration/components/item-picker/layer-picker/component-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('item-picker/layer-picker', 'Integration | Component | item picker/layer picker', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{item-picker/layer-picker}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#item-picker/layer-picker}}
+      template block text
+    {{/item-picker/layer-picker}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
