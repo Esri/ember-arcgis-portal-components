e7cff405ac3d588beea0661eb99ed6315c4d0cf2
Brian Davidson <bdavidson@esri.com>
2017-11-21 12:25:54 -0500

    add functions to component for testing

--- addon/components/item-picker/layer-picker/component.js
+++ addon/components/item-picker/layer-picker/component.js
@@ -3,4 +3,14 @@ import layout from './template';
 
 export default Ember.Component.extend({
   layout,
+  didInsertElement (test) {
+    console.log(test);
+  },
+
+  actions: {
+    test: function (value) {
+      console.log(value);
+      console.log(this);
+    }
+  }
 });
