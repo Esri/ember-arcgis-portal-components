7fc2d1978fb094be27d17671e175f46047951ef0
Brian Davidson <bdavidson@esri.com>
2017-12-05 15:06:19 -0500

    Add feature-service-preview as option for side drawer in item picker

---
+++ addon/components/item-picker/feature-service-preview/component.js
@@ -0,0 +1,71 @@
+import Ember from 'ember';
+import layout from './template';
+
+export default Ember.Component.extend({
+
+  intl: Ember.inject.service(),
+
+  layout,
+
+  classNames: [ 'item-picker-current-item-preview' ],
+
+  didInsertElement () {
+    this.$('img').on('error', Ember.run.bind(this, this.onImageError));
+  },
+
+  willDestroyElement () {
+    this.$('img').off();
+  },
+
+  didRender () {
+    // Needed to jump to error message
+    if (this.get('showError')) {
+      this.$().scrollTop(0);
+    }
+  },
+
+  itemType: Ember.computed('_i18nScope', 'model.type', function () {
+    const itemType = this.get('model.type');
+    let result = itemType;
+    const key = `${this.get('_i18nScope')}shared.itemType.${itemType.camelize()}`;
+    const intl = this.get('intl');
+    // if we don't have a translation for it, just display it as-is
+    if (intl.exists(key)) {
+      result = intl.t(key);
+    }
+    return result;
+  }),
+
+  thumbnailUrl: Ember.computed('model.thumbnail', function () {
+    const ENV = Ember.getOwner(this).resolveRegistration('config:environment');
+    const portal = ENV.APP.portalRestUrl;
+    return `${portal}/content/items/${this.get('model.id')}/info/${this.get('model.thumbnail')}`;
+  }),
+
+  thumbnailIsBroken: false,
+
+  showFallback: Ember.computed('thumbnailIsBroken', 'model.thumbnail', function () {
+    return Ember.isEmpty(this.get('model.thumbnail')) || this.get('thumbnailIsBroken');
+  }),
+
+  description: Ember.computed.reads('model.description'),
+
+  showError: Ember.computed.notEmpty('errorMessage'),
+
+  messageClass: Ember.computed('errorMessage', function () {
+    if (this.get('errorMessage.status') === 'warning') {
+      return 'alert-warning';
+    } else if (this.get('errorMessage.status') === 'error') {
+      return 'alert-danger';
+    }
+  }),
+
+  onImageError () {
+    Ember.run(this, function () {
+      if (!this.get('isDestroyed') && !this.get('isDestroying')) {
+        this.set('thumbnailIsBroken', true);
+      }
+    });
+  },
+
+});
---
+++ app/components/item-picker/feature-service-preview/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-arcgis-portal-components/components/item-picker/feature-service-preview/component';
