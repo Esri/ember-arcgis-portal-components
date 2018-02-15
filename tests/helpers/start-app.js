09f0ea46938a237e58abfe010b53d89601d5505d
mjuniper <mjuniper@gmail.com>
2018-02-15 16:53:55 -0700

    ugh

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,7 +5,7 @@ import { run } from '@ember/runloop';
 
 export default function startApp (attrs) {
   let attributes = merge({}, config.APP);
-  attributes.autoboot = true;
+  attributes.autoboot = false;
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
