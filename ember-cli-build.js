9ba3223a0352a505873e66e3f20e4d09c72fa016
Tom Wayson <tom@tomwayson.com>
2018-09-17 15:07:57 -0700

    replace {{active-link}} & dropdowns w/ {{bs-nav.dropdown}}
    NOTE: had to downgrade ember-bootstrap to ~2.0.0 due to
    
    https://github.com/kaliber5/ember-bootstrap/issues/660

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,7 +16,7 @@ module.exports = function (defaults) {
 
     'ember-cli-bootstrap-sassy': {
       // only import the bootstrap JS we use in the dummy app
-      'js': ['dropdown', 'modal']
+      'js': ['modal']
     },
 
     'ember-bootstrap': {
